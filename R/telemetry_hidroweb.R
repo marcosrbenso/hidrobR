a00d153e1065c5d8f680e86512371072573fcaf9
marcosrbenso <marcosbenso@gmail.com>
2024-06-18 17:17:48 -0300

    Add new commit

--- R/telemetry_hidroweb.R
+++ R/telemetry_hidroweb.R
@@ -36,6 +36,51 @@ telemetry <- function(CodEstacao,DataInicio,DataFim){
 }
 
 
+#' List of Telemetric stations
+#'
+#' @param Estacoes integer. 0 - active stations and 1 - in maintanance
+#' @param origem integer.  0-Todas, 1-ANA/INPE, 2-ANA/SIVAM, 3-RES_CONJ_03, 4-CotaOnline, 5-Projetos Especiais
+#'
+#' @import dplyr
+#' @import lubridate
+#' @import xml2
+#' @return
+#' @export
+#'
+#' @examples
+list_of_telemetric_stations <- function(Estacoes = 0,origem = 0){
+
+  url <- "http://telemetriaws1.ana.gov.br//ServiceANA.asmx/ListaEstacoesTelemetricas?status"
+  url2 <- paste0(
+      url,
+      "Estacoes=",
+      Estacoes,
+      "&origem=",
+      origem
+    )
+
+  xml_file <- read_xml(url2)
+
+  CodEstacao <- xml_find_all(xml_file,".//CodEstacao") %>% xml_text()
+  Operadora <- xml_find_all(xml_file,".//Operadora") %>% xml_text()
+  Bacia <- xml_find_all(xml_file,".//Bacia") %>% xml_text()
+  CodEstacao <- xml_find_all(xml_file,".//CodEstacao") %>% xml_text()
+  Latitude <- xml_find_all(xml_file,".//Latitude") %>% xml_text()
+  Longitude <- xml_find_all(xml_file,".//Longitude") %>% xml_text()
+  Altitude <- xml_find_all(xml_file,".//Altitude") %>% xml_text()
+  NomeRio <- xml_find_all(xml_file,".//NomeRio") %>% xml_text()
+  Origem <- xml_find_all(xml_file,".//Origem") %>% xml_text()
+  StatusEstacao <- xml_find_all(xml_file,".//StatusEstacao") %>% xml_text()
+
+  list_of_station <-
+    data.frame(
+      CodEstacao,Operadora,Bacia,Latitude,Longitude,Altitude,NomeRio,Origem,StatusEstacao
+    )
+}
+
+list_of_station
+
+
 
 
 
